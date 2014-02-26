87d596224b504d4ec908952efb2673bbf0b3a97a
Eric Allam <eallam@icloud.com>
2014-02-26 11:51:29 +0000

    Clean up the Test class a bit

--- Tests/ELAWalkingTimeTests.m
+++ Tests/ELAWalkingTimeTests.m
@@ -13,6 +13,8 @@
 
 #import "AsyncMacros.h"
 
+// Create a subclass of the class under test so we can override
+// the current location
 @interface ELAWalkingItemWithMockedCurrentLocation : ELAWalkingTime
 @property (assign, nonatomic) CLLocationCoordinate2D mockedCurrentLocation;
 @end
@@ -30,18 +32,6 @@
 @end
 
 @implementation ELAWalkingTimeTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
 - (void)testGetWalkingTimeInSeconds
 {
